32c11d05abcf44b7e9fb23c71e2b56fca5c61bb8
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2019-12-28 15:44:31 +0100

    Solved sixth problem.

---
+++ 0006/solution.py
@@ -0,0 +1,12 @@
+import time
+
+from utils_number_theory import sum_first_n
+
+
+start = time.process_time()
+
+sum_of_squares = sum([i**2 for i in range(1, 101)])
+square_of_sums = sum_first_n(100)**2
+
+print('Solution: ' + str(square_of_sums - sum_of_squares))
+print('\nSolution found in %s seconds.' % (time.process_time() - start))
