a18d9a02a67989bd4ea0d502e1fdc09fabd90ad1
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2019-12-29 17:53:03 +0100

    Added utils for triangular numbers.

--- utils_number_theory.py
+++ utils_number_theory.py
@@ -1,6 +1,42 @@
 def sum_first_n(n):
     """
-    Returns the sum of the first n natural numbers
+    Returns the sum of the first n natural numbers.
     """
 
     return n*(n + 1) // 2
+
+
+def gen_triangs2max(max_n):
+    """
+    Return triangular numbers up to a maximum.
+    """
+
+    if max_n < 1:
+        return []
+
+    triangs = [1]
+
+    i = 2
+    while triangs[-1] + i <= max_n:
+        triangs.append(triangs[-1] + i)
+        i += 1
+
+    return triangs
+
+
+def gen_triangs(n_triangs):
+    """
+    Returns triangular numbers up to a specific sequence length.
+    """
+
+    if not n_triangs:
+        return []
+
+    triangs = [1]
+
+    i = 2
+    while len(triangs) < n_triangs:
+        triangs.append(triangs[-1] + i)
+        i += 1
+
+    return triangs
