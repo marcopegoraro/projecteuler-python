828d5fbff29759dcf07a784c6be43a9418c6ec6d
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-01-03 17:54:06 +0100

    Improved the solution of the fifteenth problem.

--- 0015/solution.py
+++ 0015/solution.py
@@ -8,8 +8,12 @@
 # So L(n, m) = L(m, n)
 # Intuition 3: it is also easy to see that the values for L(n, m) appear in the Pascal's triangle
 # (L(n, n) is found on the center element of the (2n - 1)th row of the triangle)
+# because it is also the central binomial coefficient, which has a simple, exact non-recurrent formulation
+# (although it need the factorial).
+# Just for this time, since the numbers are very small, I will use math.factorial.
 
 import time
+from math import factorial
 
 from utils_number_theory import pascals_triangle
 
@@ -18,5 +22,6 @@ grid_y = 20
 
 start = time.process_time()
 
-print('Solution: ' + str(pascals_triangle(grid_x + 1, grid_y + 1)[grid_x][grid_y]))
+# print('Solution: ' + str(pascals_triangle(grid_x + 1, grid_y + 1)[grid_x][grid_y]))
+print('Solution: ' + str(factorial(2*grid_x) // factorial(grid_x) ** 2))
 print('\nSolution found in %s seconds.' % (time.process_time() - start))
